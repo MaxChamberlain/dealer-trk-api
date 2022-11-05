0614f53e048137184b1211147cf8c34051ad1d92
Max Chamberlain <maxchamberlain@Maxs-MacBook-Pro.local>
2022-11-05 15:12:10 -0600

    try

--- controllers/search.js
+++ controllers/search.js
@@ -19,7 +19,7 @@ const searchGurusByVin = async (req, res, next) => {
         // send an HTTP error back on page error
         page.on('pageerror', err => {
             console.log('Page error: ' + err.toString());
-            throw new Error(err);asddsf
+            throw new Error(err);
         });
         page.on('error', err => {
             console.log('Page error: ' + err.toString());
