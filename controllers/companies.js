8a6e99322c324c1a609f94659c59d483d1a81152
MaxChamberlain <97307277+MaxChamberlain@users.noreply.github.com>
2022-11-05 11:31:14 -0700

    first production test

--- controllers/companies.js
+++ controllers/companies.js
@@ -22,6 +22,58 @@ const getCompanyDetail = async (req, res) => {
     }
 };
 
+const createCompany = async (req, res) => {
+    const { user_id } = req;
+    const { company_name, company_street, company_city, company_state, company_zip, company_phone } = req.body;
+    try {
+        const pool = getDB();
+        const result = await pool.query(
+            `
+                INSERT INTO company (
+                    company_name,
+                    company_street,
+                    company_city,
+                    company_state,
+                    company_zip,
+                    company_phone
+                )
+                VALUES ($1, $2, $3, $4, $5, $6)
+                RETURNING company_id
+            `,
+            [
+                company_name,
+                company_street,
+                company_city,
+                company_state,
+                company_zip,
+                company_phone
+            ]
+        );
+        const company_id = result.rows[0].company_id;
+        const result2 = await pool.query(
+            `
+                INSERT INTO company_authorization (
+                    company_id,
+                    user_id,
+                    permission_level
+                )
+                VALUES ($1, $2, $3)
+            `,
+            [
+                company_id,
+                user_id,
+                777
+            ]
+        );
+        res.status(200).send(result.rows[0]);
+    }
+    catch (err) {
+        console.log(err)
+        res.status(500).send(err);
+    }
+};
+
 module.exports = {
     getCompanyDetail,
+    createCompany
 }
--- routes/company.js
+++ routes/company.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var router = express.Router();
 var { verifyToken } = require('../middlewares/jwt');
-var { getCompanyDetail } = require('../controllers/companies');
+var { getCompanyDetail, createCompany } = require('../controllers/companies');
 
 router.post('/getone', verifyToken, getCompanyDetail);
+router.post('/create', verifyToken, createCompany);
 
 module.exports = router;
