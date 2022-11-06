df8bf3cf08f4fd7cb8719e716815cf4b7a209921
Max Chamberlain <maxchamberlain@Maxs-MacBook-Pro.local>
2022-11-06 12:38:58 -0700

    company

--- controllers/companies.js
+++ controllers/companies.js
@@ -6,7 +6,7 @@ const getCompanyDetail = async (req, res) => {
         const pool = getDB();
         const result = await pool.query(
             `
-                SELECT *
+                SELECT DISTINCT c.company_id, c.company_name, c.company_street, c.company_city, c.company_state, c.company_zip, c.company_phone
                 FROM company_authorization ca
                 INNER JOIN company c
                 ON c.company_id = ca.company_id
@@ -14,6 +14,7 @@ const getCompanyDetail = async (req, res) => {
             `,
             [user_id]
         );
+        console.log(result.rows)
         res.status(200).send(result.rows);
     }
     catch (err) {
@@ -73,7 +74,67 @@ const createCompany = async (req, res) => {
     }
 };
 
+const addCompanyPermission = async (req, res) => {
+    const { user_id } = req;
+    const { company_id, user_email } = req.body;
+    try {
+        const pool = getDB();
+        const hasPermission = await pool.query(
+            `  
+                SELECT *
+                FROM company_authorization
+                WHERE user_id = $1
+                AND company_id = $2
+            `,
+            [
+                user_id,
+                company_id
+            ]
+        );
+        if (hasPermission.rows.length === 0) {
+            res.status(403).send('You do not have permission to add permissions to this company');
+        }
+        else {
+            const userToAdd = await pool.query(
+                `
+                    SELECT user_id
+                    FROM users
+                    WHERE user_email = $1
+                `,
+                [user_email]
+            );
+            if (userToAdd.rows.length === 0) {
+                res.status(404).send('User not found');
+            }
+            else {
+                const userToAddId = userToAdd.rows[0].user_id;
+                const result = await pool.query(
+                    `
+                        INSERT INTO company_authorization (
+                            company_id,
+                            user_id,
+                            permission_level
+                        )
+                        VALUES ($1, $2, $3)
+                    `,
+                    [
+                        company_id,
+                        userToAddId,
+                        777
+                    ]
+                );
+                res.status(200).send(result.rows[0]);
+            }
+        }
+    }
+    catch (err) {
+        console.log(err)
+        res.status(500).send(err);
+    }
+};
+
 module.exports = {
     getCompanyDetail,
-    createCompany
+    createCompany,
+    addCompanyPermission
 }
--- routes/company.js
+++ routes/company.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
 var { verifyToken } = require('../middlewares/jwt');
-var { getCompanyDetail, createCompany } = require('../controllers/companies');
+var { getCompanyDetail, createCompany, addCompanyPermission } = require('../controllers/companies');
 
 router.post('/getone', verifyToken, getCompanyDetail);
 router.post('/add', verifyToken, createCompany);
+router.post('/addpermission', verifyToken, addCompanyPermission);
 
 module.exports = router;
