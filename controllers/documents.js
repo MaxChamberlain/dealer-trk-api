721552f00cc9be9f1f8fb3b05d1d00fe620f34ef
Max Chamberlain <maxinteg@Maxs-iMac.local>
2022-11-22 00:20:30 -0700

    summary

--- controllers/documents.js
+++ controllers/documents.js
@@ -63,9 +63,35 @@ const addNotes = async (req, res) => {
     }
 };
 
+const getDocumentsByCompanyId = async (req, res) => {
+    const { company_id, user_id, startDate, endDate } = req.body;
+    try {
+        const db = getDB();
+        const companiesRef = await  db.collection('companies').doc(company_id).get();
+        if (!companiesRef) {
+            return res.status(304).send('Company not found');
+        }
+        if(!companiesRef.data().authorized_users.find(x => x.user_id === user_id)) {
+            return res.status(403).send('User not authorized');
+        }
+        const documentsRef = db.collection('documents').where('company_id', '==', company_id);
+        const documents = await documentsRef.get();
+        const user_ids = [...new Set(documents.docs.map(e => e.data().metadata.created_by_user_id))]
+        if(!user_ids.includes(user_id)) {
+            return res.status(401).send('Unauthorized');
+        }
+        res.status(200).send(documents.docs.map(doc => { return {...doc.data(), document_id: doc.id} }));
+    } catch (err) {
+        console.log(err)
+        res
+            .status(500)
+            .send(err);
+    }
+};
 
 module.exports = {
     getDocumentsByCompanyIds,
     insertDocument, 
     addNotes,
+    getDocumentsByCompanyId
 }
--- routes/document.js
+++ routes/document.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
 var { verifyToken } = require('../middlewares/jwt');
-var { getDocumentsByCompanyIds, insertDocument, addNotes } = require('../controllers/documents');
+var { getDocumentsByCompanyIds, getDocumentsByCompanyId, insertDocument, addNotes } = require('../controllers/documents');
 
 router.post('/getbycompanyids', getDocumentsByCompanyIds);
+router.post('/getbycompanyid', getDocumentsByCompanyId);
 router.post('/insert', insertDocument);
 router.post('/addnotes', addNotes);
 
