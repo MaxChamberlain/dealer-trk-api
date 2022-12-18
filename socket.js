76622639e7abcc9cf310950bb4f30531780649a4
Max Chamberlain <maxinteg@Maxs-iMac.local>
2022-12-17 17:28:37 -0700

    user colors

--- socket.js
+++ socket.js
@@ -12,14 +12,19 @@ module.exports = (io) => {
         if(!cellsBeingEditted[company_id]) cellsBeingEditted[company_id] = []
         setTimeout(() => socket.emit('init', cellsBeingEditted[company_id]), 500)
 
-        socket.on('startEditing', (data, color) => {
-            cellsBeingEditted[company_id].push(data)
-            io.to(company_id).emit('startEditing', cellsBeingEditted[company_id], color || '#ffb800');
+        socket.on('startEditing', (data, user) => {
+            if(user){
+                cellsBeingEditted[company_id] = cellsBeingEditted[company_id].filter(item => item.user !== user.user_email)
+                cellsBeingEditted[company_id].push({id: data.id, color: randomHex(user.user_email), user: user.user_email, field: data.field})
+                io.to(company_id).emit('startEditing', cellsBeingEditted[company_id]);
+            }
         });
 
-        socket.on('stopEditing', (data) => {
-            cellsBeingEditted[company_id] = cellsBeingEditted[company_id].filter(item => item.id !== data.id)
-            io.to(company_id).emit('stopEditing', cellsBeingEditted[company_id]);
+        socket.on('stopEditing', (data, user) => {
+            if(user){
+                cellsBeingEditted[company_id] = cellsBeingEditted[company_id].filter(item => item.id !== user.user_email)
+                io.to(company_id).emit('stopEditing', cellsBeingEditted[company_id]);
+            }
         });
 
         socket.on('cellChangeCommit', (data) => {
@@ -40,7 +45,7 @@ module.exports = (io) => {
                                 case 'col3': newData.data.vehicle.v_vin_no = data.params[changed]; break;
                                 case 'col4': newData.data.vehicle.v_is_certified = data.params[changed].toUpperCase() === 'Y' ? true : false; break;
                                 case 'col5': newData.data.vehicle.v_margin = isNaN(parseInt(data.params[changed])) ? newData.data.vehicle.v_margin : parseInt(data.params[changed]); break;
-                                case 'col6': newData.data.vehicle.v_days = !(isNaN(parseInt(data.params[changed]) && parseInt(data.params[changed]) >= 0)) ? newData.data.vehicle.v_days : parseInt(data.params[changed]); break;
+                                case 'col6': newData.data.vehicle.v_days = (isNaN(parseInt(data.params[changed])) || parseInt(data.params[changed]) < 0) ? newData.data.vehicle.v_days : parseInt(data.params[changed]); break;
                                 case 'col7': newData.data.vehicle.v_source = data.params[changed]; break;
                                 case 'col8': newData.data.vehicle.v_initial_mmr = isNaN(parseInt(data.params[changed])) ? newData.data.vehicle.v_initial_mmr : parseInt(data.params[changed]); break;
                                 case 'col9': newData.data.vehicle.v_final_mmr = isNaN(parseInt(data.params[changed])) ? newData.data.vehicle.v_final_mmr : parseInt(data.params[changed]); break;
@@ -87,30 +92,17 @@ module.exports = (io) => {
                 console.log(e)
             }
         })
-		socket.on('disconnect', () => console.log('disconnected')); 
+		socket.on('disconnect', (user) => {
+            cellsBeingEditted[company_id] = cellsBeingEditted[company_id].filter(cell => cell.socket_id !== user.user_email)
+            io.to(company_id).emit('stopEditing', cellsBeingEditted[company_id])
+            console.log('disconnected')
+        }); 
 
     })
     
 }
 
 const insertNew = (data, company_id, io) => { 
-    // case 'col1': newData.data.vehicle.v_stock_no = data.params[changed]; break;
-    // case 'col2': newData.data.vehicle.v_vehicle = data.params[changed]; break;
-    // case 'col3': newData.data.vehicle.v_vin_no = data.params[changed]; break;
-    // case 'col4': newData.data.vehicle.v_is_certified = data.params[changed].toUpperCase() === 'Y' ? true : false; break;
-    // case 'col5': newData.data.vehicle.v_margin = isNaN(parseInt(data.params[changed])) ? newData.data.vehicle.v_margin : parseInt(data.params[changed]); break;
-    // case 'col6': newData.data.vehicle.v_days = !(isNaN(parseInt(data.params[changed]) && parseInt(data.params[changed]) >= 0)) ? newData.data.vehicle.v_days : parseInt(data.params[changed]); break;
-    // case 'col7': newData.data.vehicle.v_source = data.params[changed]; break;
-    // case 'col8': newData.data.vehicle.v_initial_mmr = isNaN(parseInt(data.params[changed])) ? newData.data.vehicle.v_initial_mmr : parseInt(data.params[changed]); break;
-    // case 'col9': newData.data.vehicle.v_final_mmr = isNaN(parseInt(data.params[changed])) ? newData.data.vehicle.v_final_mmr : parseInt(data.params[changed]); break;
-    // case 'col10': newData.data.vehicle.v_initial_carg_h = isNaN(parseInt(data.params[changed])) ? newData.data.vehicle.v_initial_carg_h : parseInt(data.params[changed]); break;
-    // case 'col11': newData.data.vehicle.v_final_carg_h = isNaN(parseInt(data.params[changed])) ? newData.data.vehicle.v_final_carg_h : parseInt(data.params[changed]); break;
-    // case 'col12': newData.data.vehicle.v_start_price = isNaN(parseInt(data.params[changed])) ? newData.data.vehicle.v_start_price : parseInt(data.params[changed]); break;
-    // case 'col13': newData.data.vehicle.v_sell_price = isNaN(parseInt(data.params[changed])) ? newData.data.vehicle.v_sell_price : parseInt(data.params[changed]); break;
-    // case 'col14': newData.data.vehicle.v_market_percent = isNaN(parseInt(data.params[changed])) ? newData.data.vehicle.v_market_percent : parseInt(data.params[changed]); break;
-    // case 'col15': newData.data.trade.t_vehicle = data.params[changed]; break;
-    // case 'col16': newData.metadata.created_at = isNaN(Date.parse(data.params[changed])) ? newData.metadata.created_at : new Date(data.params[changed]).toLocaleDateString('en-US'); break;
-    // case 'col17': newData.notes = data.params[changed]; break;
     data = data.params
     const db = getDB();
     if(Object.keys(data).some(key => data[key] === '')){
@@ -174,4 +166,51 @@ const insertNew = (data, company_id, io) => {
         
         })
     }
+}
+
+function randomHSL(str) {
+    let hash = 0;
+    for (let i = 0; i < str.length; i++) {
+      hash = str.charCodeAt(i) + ((hash << 5) - hash);
+    }
+    const h = hash % 360;
+    const s = 50 + Math.abs(hash % 20);
+    const l = 50 + Math.abs(hash % 20);
+    return `hsl(${h}, ${s}%, ${l}%)`;
+  }
+
+function randomHex(email){
+    // extract the last number of email address
+    let num = parseInt(email.slice(email.indexOf('@') - 2, email.indexOf('@') - 1).charCodeAt())
+    let index = num % 22;
+    num = num % 10;
+    num = num * 100
+    // get the color name
+    let colors = [
+      "Slate",
+      "Gray",
+      "Zinc",
+      "Neutral",
+      "Stone",
+      "Red",
+      "Orange",
+      "Amber",
+      "Yellow",
+      "Lime",
+      "Green",
+      "Emerald",
+      "Cyan",
+      "Teal",
+      "Sky",
+      "Blue",
+      "Indigo",
+      "Violet",
+      "Purple",
+      "Fucshia",
+      "Pink",
+      "Rose",
+    ];
+    // return the color
+    let color = colors[index % 22].toLowerCase()
+    return `${color}-${num}`;
 }
